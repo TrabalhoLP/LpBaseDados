87f538e563de6f2668bd77615e68a0392000ec8b
cristianamodesto <65223354+cristianamodesto@users.noreply.github.com>
2020-05-21 16:12:10 +0100

    Update Registo.java

--- src/programa/Registo.java
+++ src/programa/Registo.java
@@ -2,4 +2,37 @@ package programa;
 
 public class Registo {
 
+	private String [] arrayRegisto;
+	
+	public Registo(int tamanho) {
+		arrayRegisto= new String [tamanho];
+	}
+
+	public String[] getArrayRegisto() {
+		return arrayRegisto;
+	}
+
+	public void setArrayRegisto(String[] arrayRegisto) {
+		this.arrayRegisto = arrayRegisto;
+	}
+	
+	public void adicionarRegisto(String registo) {
+			deslocarDireita();
+			arrayRegisto[0]=registo;
+		
+		}
+		
+		
+	private void deslocarDireita() {
+	String [] novo= new String[arrayRegisto.length+1];
+	int i=1;
+	for(String registo: arrayRegisto) {
+		novo[i]=registo;
+		i++;
+	}
+	arrayRegisto=novo;
 }
+
+	}
+	
+
