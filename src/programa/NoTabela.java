257e19105ac328c46ff5053a9239cb3d6466f34e
Nadia Nobre <mimix25@gmail.com>
2020-05-21 14:51:28 +0100

    Class NoTabela 1.0

--- src/programa/NoTabela.java
+++ src/programa/NoTabela.java
@@ -3,17 +3,38 @@ package programa;
 public class NoTabela {
 	
 	private String nome;
-	private Registo [] registos;
 	
+	private Registo [] registos;
+	private int contaRegisto;
 	private String [] colunas;
 	
 	public NoTabela(String nome, int numColunas) {
 		this.nome = nome;
 		this.colunas = new String [numColunas]; 
+		this.contaRegisto = 0;
 		/* este contrutor recebe o nome da tabela 
 		 * e o numero de colunas(campos) que a tabela tem
 		 * Este NumColunas, serve para criar uma lista String colunas
 		 * Para que o utilizador depois diga o nome de cada campo */
-		
 	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String[] getColunas() {
+		
+		return colunas;
+	}
+	
+	public int getRegistos() {
+		
+		return this.contaRegisto;
+	}
+
+	
 }
