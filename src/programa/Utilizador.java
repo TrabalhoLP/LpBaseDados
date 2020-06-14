9c204fa90c32c744e5d344046ba111569b424add
Nadia Nobre <mimix25@gmail.com>
2020-06-14 08:53:09 +0100

    Eliminar, consultar, criar Registo por chaveValor a funcionar

--- src/programa/Utilizador.java
+++ src/programa/Utilizador.java
@@ -29,7 +29,7 @@ public class Utilizador {
 	}
 
 	// Há um utilizador no array? temos de criar um user
-	
+
 	public String procurarUsername(String username, String password) {// procurar username no arraylist onde se
 																		// encontram os usernames
 		for (int i = 0; i < users.size(); i++) {
@@ -70,7 +70,7 @@ public class Utilizador {
 		if (opcao == 1) {
 			// introduz a password
 			System.out.println(users);
-			//ler();
+			// ler();
 			menuUtilizador();
 
 		}
@@ -127,7 +127,6 @@ public class Utilizador {
 
 	}
 
-
 	public void menuUtilizador() {
 		saveData();
 		System.out.println(arvore.getTabela("Tabela1"));
@@ -142,7 +141,7 @@ public class Utilizador {
 		if (opcao == 2) {
 			iniciar();
 		}
-		
+
 	}
 
 	private void obterRegistos() {// caso seja selecionada a primeira opcao
@@ -158,23 +157,23 @@ public class Utilizador {
 			System.out.println("CRIAR REGISTO");
 			ler.nextLine();
 			query = ler.nextLine();
-			if (query.compareTo("CONSULTAR REGISTO")==0) {// o que fazer no caso da escrita deste comando
+			if (query.compareTo("CONSULTAR REGISTO") == 0) {// o que fazer no caso da escrita deste comando
 				consultarRegistos();
-			} else if (query.compareTo("ELIMINAR REGISTO")==0) { // o que fazer no caso da escrita deste comando
+			} else if (query.compareTo("ELIMINAR REGISTO") == 0) { // o que fazer no caso da escrita deste comando
 				eliminarRegisto();
-			} else if (query.compareTo("CRIAR REGISTO")==0) {
+			} else if (query.compareTo("CRIAR REGISTO") == 0) {
 				System.out.println("Qual o nome da tabela onde pretende criar o registo?");
-				String nomeTabela=ler.nextLine();
-				System.out.println("Qual o nome do registo que pretende criar?");
-				String nomeRegisto=ler.nextLine();
-				 criarRegisto(nomeTabela,nomeRegisto);
-				 
-			} else if (query.compareTo("CRIAR TABELA")==0) { // o que fazer no caso da escrita deste comando
+				String nomeTabela = ler.nextLine();
+				System.out.println("Para deixar de introduzir Escreva EXIT\n");
+
+				criarRegisto(nomeTabela);
+
+			} else if (query.compareTo("CRIAR TABELA") == 0) { // o que fazer no caso da escrita deste comando
 				System.out.println("Introduza o nome da tabela");
 				String nomeTabela = ler.next();
 				criarTabela(nomeTabela);
 
-			} else if (query.compareTo("CONSULTAR TABELA")==0) {
+			} else if (query.compareTo("CONSULTAR TABELA") == 0) {
 				System.out.println("Nome da tabela a consultar");
 				String nome = ler.next();
 				consultarTabela(nome);
@@ -205,85 +204,131 @@ public class Utilizador {
 	 */
 
 	private void consultarTabela(String nome) {
-		
-		NoTabela tabela = arvore.getTabela(nome); // vai buscar ao metodo da arvore que ainda não esta totalmente otimizado
+
+		NoTabela tabela = arvore.getTabela(nome); // vai buscar ao metodo da arvore que ainda não esta totalmente
+													// otimizado
 		System.out.println(tabela.toString());
 		menuUtilizador();
 	}
-	
+
 	private void criarTabela(String nome) {
 		arvore.criaTabela(nome);
 		menuUtilizador();
 
 	}
-	
-	private void criarRegisto(String nomeTabela,String nomeRegisto) {
-		 int elementos;
-		 
-		 String valorChave="";
-		 String valor="";
-		 Registo registoAtual = new Registo();
-		 registoAtual.setNome(nomeRegisto);
-		 registoAtual.setUniqueID();
-		 
 
-		 while (valor.compareTo("EXIT")!=0 ) {
-			 
-			 System.out.println("Nome do Campo?");
-			 valorChave = ler.nextLine();
-			 if(valorChave.compareTo("EXIT")==0){return;}
-			 System.out.println("Valor?");
-			 valor = ler.nextLine();
-			 
-			 if(valorChave.compareTo("EXIT")!=0||valor.compareTo("EXIT")!=0) {
-				 if (valorChave.compareTo("")==0) {
-					 valorChave = null;
-				 }
-				 if (valor.compareTo("")==0){
-					 valor = null;
-				 }
-				 
-				 registoAtual.getArrayRegisto().add(new ChaveValor (valorChave, valor));
+	private void criarRegisto(String nomeTabela) {
+		int elementos;
 
-			 }
-		 }
-		 
-		 arvore.getTabela(nomeTabela).getRegistos().add(registoAtual);
-		 System.out.println("Registo criado com sucesso!");
-		 menuUtilizador();
+		String valorChave = "";
+		String valor = "";
+		Registo registoAtual = new Registo();
+		registoAtual.setUniqueID();
+
+		while (valor.compareTo("EXIT") != 0) {
+
+			System.out.println("Nome do Campo?");
+			valorChave = ler.nextLine();
+			if (valorChave.compareTo("EXIT") == 0) {
+				arvore.getTabela(nomeTabela).getRegistos().add(registoAtual);
+				System.out.println("Registo criado com sucesso!");
+				menuUtilizador();
+			}
+			System.out.println("Valor?");
+			valor = ler.nextLine();
+
+			if (valorChave.compareTo("EXIT") != 0 || valor.compareTo("EXIT") != 0) {
+				if (valorChave.compareTo("") == 0) {
+					valorChave = null;
+				}
+				if (valor.compareTo("") == 0) {
+					valor = null;
+				}
+
+				registoAtual.getArrayRegisto().add(new ChaveValor(valorChave, valor));
+
+			}
+		}
+
+		arvore.getTabela(nomeTabela).getRegistos().add(registoAtual);
+		System.out.println("Registo criado com sucesso!");
+		menuUtilizador();
 	}
 
 	private void consultarRegistos() {
 		String nomeRegisto, nomeTabela;
+		int escolha;
 		System.out.println("Qual o nome da tabela que pretende consultar?");
-		nomeTabela=ler.nextLine();
+		nomeTabela = ler.nextLine();
 		System.out.println("Introduz chave e depois valor chave:valor separado com :");
-		nomeRegisto=ler.nextLine();
-		
-		menuUtilizador();
-		}
+		nomeRegisto = ler.nextLine();
+		String chaveEvalor[] = splice(nomeRegisto);
 
-	private void eliminarRegisto() {
-		String nomeTabela,nomeRegisto;
-		System.out.println("Qual o nome da tabela na qual pretende eliminar o registo?");
-		nomeTabela=ler.nextLine();
-		System.out.println("chave:valor separa a chave do valor com : ");
-		nomeRegisto=ler.nextLine();
-		
 		ArrayList TodosRegistos = arvore.getTabela(nomeTabela).getRegistos();
-		
-		for (int i=0; i<TodosRegistos.size(); i++) {
+
+		for (int i = 0; i < TodosRegistos.size(); i++) {
 			Registo registoAtual = (Registo) TodosRegistos.get(i);
 			ArrayList dados = registoAtual.getArrayRegisto();
 
-			for (int y=0; y<registoAtual.getArrayRegisto().size(); y++) {
+			for (int y = 0; y < registoAtual.getArrayRegisto().size(); y++) {
 				ChaveValor dado = (ChaveValor) dados.get(y);
-				System.out.println(dado.toString()); // aqui é que temos de comparar a chave com a chave que recebemos;
+
+				if (dado.getChave().compareTo(chaveEvalor[0]) == 0) {
+					if (dado.getValor().compareTo(chaveEvalor[1]) == 0) {
+						System.out.println("enter para sair");
+						System.out.println(dados.toString()+"\n--------------------------------------"); // aqui é que temos de comparar a chave com a chave que
+					}
+				} else {
+					System.out.println("Nada encontrado");
+				}
+
 			}
 		}
-		
+
+		menuUtilizador();
+
 	}
-	
+
+	private void eliminarRegisto() {
+		int escolha;
+		String nomeTabela, nomeRegisto;
+		System.out.println("Qual o nome da tabela na qual pretende eliminar o registo?");
+		nomeTabela = ler.nextLine();
+		System.out.println("chave:valor separa a chave do valor com : ");
+		nomeRegisto = ler.nextLine();
+
+		String chaveEvalor[] = splice(nomeRegisto);
+
+		ArrayList TodosRegistos = arvore.getTabela(nomeTabela).getRegistos();
+
+		for (int i = 0; i < TodosRegistos.size(); i++) {
+			Registo registoAtual = (Registo) TodosRegistos.get(i);
+			ArrayList dados = registoAtual.getArrayRegisto();
+
+			for (int y = 0; y < registoAtual.getArrayRegisto().size(); y++) {
+				ChaveValor dado = (ChaveValor) dados.get(y);
+
+				if (dado.getChave().compareTo(chaveEvalor[0]) == 0) {
+					if (dado.getValor().compareTo(chaveEvalor[1]) == 0) {
+						System.out.println("Encontramos este registo deseja eliminar? 0-Para não  ----  1-para sim");
+						System.out.println(dados.toString()); // aqui é que temos de comparar a chave com a chave que
+																// recebemos;
+						escolha = ler.nextInt();
+						if (escolha == 1) {
+							dados.remove(y); // remover o objecto que lá esta
+						} else {
+							return;
+						}
+					}
+				} else {
+					System.out.println("Nada encontrado");
+				}
+
+			}
+		}
+		menuUtilizador();
+	}
+
 	public void saveData() {
 
 		try {
@@ -302,60 +347,44 @@ public class Utilizador {
 		}
 
 	}
-/*	public void deleteData() throws IOException {
-		// PrintWriter object for output.txt 
-        PrintWriter pw = new PrintWriter("dados.txt"); 
-          
-        // BufferedReader object for input.txt 
-        BufferedReader br1 = new BufferedReader(new FileReader("dados.txt")); 
-          
-        String line1 = br1.readLine(); 
-          
-        // loop for each line of input.txt 
-        while(line1 != null) 
-        { 
-            boolean flag = false; 
-              
-            // BufferedReader object for delete.txt 
-            BufferedReader br2 = new BufferedReader(new FileReader("dados.txt")); 
-              
-            String line2 = br2.readLine(); 
-              
-            // loop for each line of delete.txt 
-            while(line2 != null) 
-            { 
-                if(line1.equals(line2)) 
-                { 
-                    flag = true; 
-                    break; 
-                } 
-                  
-                line2 = br2.readLine(); 
-            } 
-              
-            // if flag = false 
-            // write line of input.txt to output.txt 
-            if(!flag) 
-                pw.println(line1); 
-              
-            line1 = br1.readLine(); 
-              
-        } 
-          
-        pw.flush(); 
-          
-        // closing resources 
-        br1.close(); 
-        pw.close(); 
-          
-        System.out.println("File operation performed successfully"); 
-    }*/
-        
-  
-	
-	
+	/*
+	 * public void deleteData() throws IOException { // PrintWriter object for
+	 * output.txt PrintWriter pw = new PrintWriter("dados.txt");
+	 * 
+	 * // BufferedReader object for input.txt BufferedReader br1 = new
+	 * BufferedReader(new FileReader("dados.txt"));
+	 * 
+	 * String line1 = br1.readLine();
+	 * 
+	 * // loop for each line of input.txt while(line1 != null) { boolean flag =
+	 * false;
+	 * 
+	 * // BufferedReader object for delete.txt BufferedReader br2 = new
+	 * BufferedReader(new FileReader("dados.txt"));
+	 * 
+	 * String line2 = br2.readLine();
+	 * 
+	 * // loop for each line of delete.txt while(line2 != null) {
+	 * if(line1.equals(line2)) { flag = true; break; }
+	 * 
+	 * line2 = br2.readLine(); }
+	 * 
+	 * // if flag = false // write line of input.txt to output.txt if(!flag)
+	 * pw.println(line1);
+	 * 
+	 * line1 = br1.readLine();
+	 * 
+	 * }
+	 * 
+	 * pw.flush();
+	 * 
+	 * // closing resources br1.close(); pw.close();
+	 * 
+	 * System.out.println("File operation performed successfully"); }
+	 */
+
 	public void iniciaDados() {
-		
+
 		try {
 
 			FileInputStream fi = new FileInputStream(new File("dados.txt"));
@@ -375,17 +404,37 @@ public class Utilizador {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		System.out.println("Dados carregados com sucesso..");
 		System.out.println(arvore.getTabela("Tabela1"));
-		System.out.println(arvore.getTabela("Tabela1").getRegistos().get(0).getClass());
 		/*
-		Registo cena = (Registo) arvore.getTabela("Tabela1").getRegistos().get(0);
-		ChaveValor chave = (ChaveValor) cena.getArrayRegisto().get(0);
-		System.out.println(chave.getChave());
-		 */
+		System.out.println(arvore.getTabela("Tabela1").getRegistos().get(0).getClass());
 		
+		 * Registo cena = (Registo) arvore.getTabela("Tabela1").getRegistos().get(0);
+		 * ChaveValor chave = (ChaveValor) cena.getArrayRegisto().get(0);
+		 * System.out.println(chave.getChave());
+		 */
+
 	}
 
+	public String[] splice(String frase) {
+
+		int start = 0, end = 0, start1 = 0, end1 = 0;
+		String resultado[] = new String[2];
+
+		for (int a = 0; a < frase.length(); a++) {
+			if (frase.charAt(a) == ':') {
+				start = 0;
+				end = a;
+				start1 = a + 1;
+				end1 = frase.length();
+			}
+		}
+
+		resultado[0] = frase.substring(start, end);
+		resultado[1] = frase.substring(start1, end1);
+		return resultado;
+
+	}
 
 }
